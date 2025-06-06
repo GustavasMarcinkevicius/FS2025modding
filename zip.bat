a1e8d76ca03c95b7f2fab998682507df233623a5
Gustavas <Gustavas.marcinkevicius@gmail.com>
2025-06-06 20:11:26 +0300

    added mod, now in 'chainsaws'

--- zip.bat
+++ zip.bat
@@ -1,5 +1,12 @@
 @echo off
-set MODNAME=SmartAgrometer
-echo Creating %MODNAME%.zip...
-powershell -Command "Compress-Archive -Path * -DestinationPath %MODNAME%.zip -Force"
-echo Done.
+set MODNAME=FS25_SmartAgrometer
+set TARGETDIR=%USERPROFILE%\Documents\My Games\FarmingSimulator2025\mods
+
+echo Creating zip archive...
+powershell -Command "Compress-Archive -Path * -DestinationPath '%MODNAME%.zip' -Force"
+
+echo Moving to mods folder...
+move /Y "%MODNAME%.zip" "%TARGETDIR%\%MODNAME%.zip"
+
+echo Done. Mod deployed to FS25 mods folder.
+
