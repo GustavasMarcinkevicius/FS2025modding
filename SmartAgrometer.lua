b5b1d13f4c5d527f33a12903f95291695034955c
Gustavas <Gustavas.marcinkevicius@gmail.com>
2025-06-07 19:32:18 +0300

    added check to not re-create the same file everytime the game launches

--- SmartAgrometer.lua
+++ SmartAgrometer.lua
@@ -1,6 +1,22 @@
-print("DEBUG: SmartAgrometer script started")
+print("DEBUG: SmartAgrometer script loaded")
 
 SmartAgrometer = {}
+
+local chemicalElements = {
+    "Nitrogen", "Phosphorus", "Potassium", "Calcium",
+    "Magnesium", "Sulfur", "Iron", "Zinc",
+    "Copper", "Manganese", "Boron", "Molybdenum"
+}
+
+local function fileExists(path)
+    local file = io.open(path, "r")
+    if file then
+        file:close()
+        return true
+    else
+        return false
+    end
+end
 
 function SmartAgrometer:update(dt)
     if not self.loadFinished and g_currentMission ~= nil and g_currentMission.isLoaded then
@@ -8,60 +24,58 @@ function SmartAgrometer:update(dt)
 
         local saveDir = nil
         if g_currentMission.missionInfo ~= nil then
-            saveDir = g_currentMission.missionInfo.savegameDirectory
-            if saveDir == nil then
-                saveDir = g_currentMission.missionInfo.savegameName
-            end
+            saveDir = g_currentMission.missionInfo.savegameDirectory or g_currentMission.missionInfo.savegameName
         end
 
         print("DEBUG: saveDir = " .. tostring(saveDir))
 
-        local filePath = nil
-        if saveDir ~= nil then
-            -- Check if saveDir is an absolute path (Windows example)
-            if string.match(saveDir, "^[a-zA-Z]:[/\\]") then
-                filePath = saveDir .. "/SmartAgrometerData.xml"
-            else
-                local baseSavePath = getUserProfileAppPath() .. "savegames/"
-                filePath = baseSavePath .. saveDir .. "/SmartAgrometerData.xml"
-            end
-
-            print("DEBUG: Full file path: " .. filePath)
-
-            if g_farmlandManager ~= nil and g_farmlandManager.farmlands ~= nil then
-                local numFarmlands = #g_farmlandManager.farmlands
-                print("DEBUG: Number of farmlands = " .. tostring(numFarmlands))
-
-                local chemicalElements = {
-                    "Nitrogen", "Phosphorus", "Potassium", "Calcium",
-                    "Magnesium", "Sulfur", "Iron", "Zinc",
-                    "Copper", "Manganese", "Boron", "Molybdenum"
-                }
-
-                local xmlFile = createXMLFile("SmartAgrometerData", filePath, "farmlands")
-
-                for i = 1, numFarmlands do
-                    local farmland = g_farmlandManager.farmlands[i]
-                    if farmland ~= nil then
-                        local randomElement = chemicalElements[math.random(#chemicalElements)]
-                        local keyBase = string.format("farmlands.farmland(%d)", i - 1) -- zero-based index for XML
-                        setXMLInt(xmlFile, keyBase .. "#id", i)  -- just assign ID = i
-                        setXMLString(xmlFile, keyBase .. ".chemicalElement", randomElement)
-                        print(string.format("DEBUG: Farmland %d assigned element %s", i, randomElement))
-                    else
-                        print(string.format("DEBUG: Farmland %d is nil", i))
-                    end
-                end
-
-                saveXMLFile(xmlFile)
-                delete(xmlFile)
-                print("DEBUG: XML file saved at " .. filePath)
-            else
-                print("DEBUG: g_farmlandManager or farmlands is nil")
-            end
-        else
+        if saveDir == nil then
             print("DEBUG: Save directory is nil, cannot create XML file")
+            return
         end
+
+        local filePath = nil
+        if string.match(saveDir, "^[a-zA-Z]:[/\\]") then
+            -- absolute path (Windows)
+            filePath = saveDir .. "/SmartAgrometerData.xml"
+        else
+            -- relative path, construct from base savegames folder
+            local baseSavePath = getUserProfileAppPath() .. "savegames/"
+            filePath = baseSavePath .. saveDir .. "/SmartAgrometerData.xml"
+        end
+
+        print("DEBUG: Full file path: " .. filePath)
+
+        if fileExists(filePath) then
+            print("DEBUG: XML file already exists at " .. filePath .. ", skipping creation.")
+            return
+        end
+
+        if g_farmlandManager == nil or g_farmlandManager.farmlands == nil then
+            print("DEBUG: g_farmlandManager or farmlands table is nil, cannot proceed")
+            return
+        end
+
+        local numFarmlands = #g_farmlandManager.farmlands
+        print("DEBUG: Number of farmlands = " .. tostring(numFarmlands))
+
+        local xmlFile = createXMLFile("SmartAgrometerData", filePath, "farmlands")
+
+        for i = 1, numFarmlands do
+            local farmland = g_farmlandManager.farmlands[i]
+            local farmlandId = i  -- just use 1-based index as ID
+            local randomElement = chemicalElements[math.random(#chemicalElements)]
+
+            local keyBase = string.format("farmlands.farmland(%d)", i - 1) -- XML uses 0-based index
+
+            setXMLInt(xmlFile, keyBase .. "#id", farmlandId)
+            setXMLString(xmlFile, keyBase .. ".chemicalElement", randomElement)
+        end
+
+        saveXMLFile(xmlFile)
+        delete(xmlFile)
+
+        print("DEBUG: XML file created and saved at " .. filePath)
     end
 end
 
