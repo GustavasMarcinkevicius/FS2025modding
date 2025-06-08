ed2ebff810f1cc7f4b6442464c26d3ce5b82161f
Gustavas <Gustavas.marcinkevicius@gmail.com>
2025-06-08 13:54:36 +0300

    working fertilizer texture

--- handToolFunction.lua
+++ handToolFunction.lua
@@ -1,61 +1,36 @@
-print("DEBUG: HANDTOOLFUNCTION.LUA ACCESSED")
+print("DEBUG: SmartAgrometerHandTool class loaded")
 
 SmartAgrometerHandTool = {}
 local SmartAgrometerHandTool_mt = Class(SmartAgrometerHandTool, HandTool)
+InitObjectClass(SmartAgrometerHandTool, "SmartAgrometerHandTool")
+
+if allowInput and g_inputBinding:getDigitalInputJustPressed(InputAction.SMARTAGROMETER_SCAN) then
+    print("DEBUG: Left click detected!")
+    self:scanFieldUnderPlayer()
+end
 
 function SmartAgrometerHandTool:new(isServer, isClient)
+    print("DEBUG: SmartAgrometerHandTool:new() called")
     local self = HandTool:new(isServer, isClient)
     setmetatable(self, SmartAgrometerHandTool_mt)
     return self
 end
 
-function SmartAgrometerHandTool:onActivate()
-    print("DEBUG: onActivate() called!")
-    SmartAgrometerHandTool:superClass().onActivate(self)  -- call base onActivate
+function SmartAgrometerHandTool:onEquip()
+    print("DEBUG: SmartAgrometerHandTool equipped!")
+end
 
-    -- Now your custom code here:
-    local farmlandId = SmartAgrometer:getFarmlandUnderPlayer()
-    if farmlandId ~= nil then
-        local element = SmartAgrometer:scanField(farmlandId)
-        if element ~= nil then
-            g_currentMission:addIngameNotification(FSBaseMission.INGAME_NOTIFICATION_OK, "Chemical element: " .. element)
-        else
-            g_currentMission:addIngameNotification(FSBaseMission.INGAME_NOTIFICATION_ERROR, "No chemical element data for field")
-        end
-    else
-        g_currentMission:addIngameNotification(FSBaseMission.INGAME_NOTIFICATION_ERROR, "You are not on any farmland")
+function SmartAgrometerHandTool:update(dt, allowInput)
+    SmartAgrometerHandTool:superClass().update(self, dt, allowInput)
+    print("DEBUG: SmartAgrometerHandTool:update")
+
+    if allowInput and g_inputBinding:getDigitalInputJustPressed(InputAction.SMARTAGROMETER_SCAN) then
+        print("DEBUG: Scan input triggered!")
+        self:scanFieldUnderPlayer()
     end
 end
--- SmartAgrometerHandTool = {}
--- local SmartAgrometerHandTool_mt = Class(SmartAgrometerHandTool, HandTool)
 
--- function SmartAgrometerHandTool:new(isServer, isClient)
---     local self = HandTool:new(isServer, isClient)
---     setmetatable(self, SmartAgrometerHandTool_mt)
---     return self
--- end
-
--- -- Override activate() to add your behavior
--- function SmartAgrometerHandTool:activate()
---     print("DEBUG: activate() called")
---     -- Call the base class activate() so the game handles activation properly
---     SmartAgrometerHandTool:superClass().activate(self)
-    
---     -- Run your custom logic when the tool is activated (used)
---     self:onActivate()
--- end
-
--- function SmartAgrometerHandTool:onActivate()
---     print("DEBUG: onActivate() called")
---     local farmlandId = SmartAgrometer:getFarmlandUnderPlayer()
---     if farmlandId ~= nil then
---         local element = SmartAgrometer:scanField(farmlandId)
---         if element ~= nil then
---             g_currentMission:addIngameNotification(FSBaseMission.INGAME_NOTIFICATION_OK, "Chemical element: " .. element)
---         else
---             g_currentMission:addIngameNotification(FSBaseMission.INGAME_NOTIFICATION_ERROR, "No chemical element data for field")
---         end
---     else
---         g_currentMission:addIngameNotification(FSBaseMission.INGAME_NOTIFICATION_ERROR, "You are not on any farmland")
---     end
--- end
+function SmartAgrometerHandTool:scanFieldUnderPlayer()
+    print("DEBUG: Scanning field!")
+    g_currentMission:addIngameNotification(FSBaseMission.INGAME_NOTIFICATION_OK, "Scan complete!")
+end
