40c7f8c89e5853c1b3a8a7cbd132c9b0ffd3aa55
Gustavas <Gustavas.marcinkevicius@gmail.com>
2025-06-08 22:02:22 +0300

    added pngs to fertilizers, tool still not working

--- handToolFunction.lua
+++ handToolFunction.lua
@@ -1,17 +1,31 @@
 print("DEBUG: SmartAgrometerHandTool class loaded")
-print("DEBUG: InputAction.SMARTAGROMETER_SCAN =", InputAction.SMARTAGROMETER_SCAN)
 
 SmartAgrometerHandTool = {}
 local SmartAgrometerHandTool_mt = Class(SmartAgrometerHandTool, Flashlight)
 InitObjectClass(SmartAgrometerHandTool, "SmartAgrometerHandTool")
 
+function SmartAgrometerHandTool:new(isServer, isClient)
+    local self = Flashlight:new(isServer, isClient)
+    setmetatable(self, SmartAgrometerHandTool_mt)
+    return self
+end
+
 function SmartAgrometerHandTool:update(dt, allowInput)
     SmartAgrometerHandTool:superClass().update(self, dt, allowInput)
 
-    if InputAction.SMARTAGROMETER_SCAN == nil then
-        print("DEBUG: InputAction.SMARTAGROMETER_SCAN is nil during update")
+    -- Check if InputAction is available yet
+    if InputAction == nil then
+        print("DEBUG: InputAction is nil during update")
+        return
     end
 
+    -- Check if your custom input action exists
+    if InputAction.SMARTAGROMETER_SCAN == nil then
+        print("DEBUG: InputAction.SMARTAGROMETER_SCAN is nil during update")
+        return
+    end
+
+    -- Check input and trigger scan
     if allowInput and g_inputBinding:getDigitalInputJustPressed(InputAction.SMARTAGROMETER_SCAN) then
         print("DEBUG: SMARTAGROMETER_SCAN triggered")
         self:scanFieldUnderPlayer()
